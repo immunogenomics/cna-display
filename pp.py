9f622e70f5179fde266137e5ec57f0dbae609d13
Yakir Reshef <yr073@cn002.research.partners.org>
2021-01-21 16:35:06 -0500

    nonnull figs

--- pp.py
+++ pp.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 
 plt.rc('axes', linewidth=0.8)
 
-# aesthetics
 labelfontsize=8
 tickprops = {
         'direction':'out',
@@ -10,18 +9,6 @@ tickprops = {
         'width':0.8,
         'pad':4,
         'labelsize':7}
-sig_thresh_line_props = {
-        'color':'gray',
-        'linestyle':'--',
-        'linewidth':0.5,
-        'alpha':0.8
-        }
-enrichment_thresh_line_props = {
-        'color':'darkgray',
-        'linestyle':'-',
-        'linewidth':1,
-        'alpha':1
-        }
 qqprops = {
         's':1.5,
         'fontsize':labelfontsize,

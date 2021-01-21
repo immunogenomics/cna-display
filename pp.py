87a99578699e9eb026aa91c19beb1331320f90fe
Yakir Reshef <yr073@cn002.research.partners.org>
2021-01-21 18:20:25 -0500

    tbru unsupervised fig in progress, mpl style file

--- pp.py
+++ pp.py
@@ -1,16 +1,4 @@
-import matplotlib.pyplot as plt
-
-plt.rc('axes', linewidth=0.8)
-
-labelfontsize=8
-tickprops = {
-        'direction':'out',
-        'length':2,
-        'width':0.8,
-        'pad':4,
-        'labelsize':7}
 qqprops = {
         's':1.5,
-        'fontsize':labelfontsize,
-        'linewidth':0}
-
+        'linewidth':0
+}

027f689772b844e0fee9c670e3c7d3167e691c19
rumker <lar24@cn002.research.partners.org>
2021-03-02 01:09:27 -0500

    Added scatter_random back into pp.y

--- pp.py
+++ pp.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 qqprops = {
         's':1.5,
         'linewidth':0
@@ -8,3 +11,21 @@ umapprops = {
     's':2,
     'rasterized':True
 }
+
+def scatter_random(x, y, c=None, symmetrize=True, percentile=90, ax=None, seed=0, **kwargs):
+    if ax is None:
+        ax = plt.gca()
+    if c is None:
+        print('error: no color supplied')
+        return
+    kwargs.update(umapprops)
+    
+    ix = np.argsort(np.random.randn(len(x)))
+    x = x[ix]
+    y = y[ix]
+    c = c[ix]
+    
+    if symmetrize:
+        maxval = np.percentile(np.abs(c), percentile)
+        kwargs.update({'vmin':-maxval, 'vmax':maxval})
+    ax.scatter(x, y, c=c, **kwargs)

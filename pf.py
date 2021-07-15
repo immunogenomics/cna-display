b37e73c6b5f25e71d57d17b237db35252fad2213
Yakir Reshef <yr073@cn002.research.partners.org>
2021-07-15 11:08:59 -0400

    finalized sepsis simulation

--- pf.py
+++ pf.py
@@ -2,7 +2,7 @@ data = '/data/srlab1/laurie-yakir/'
 tbru = data + 'tbru.real/'
 tbru_h5ad = tbru + '2.munged/'
 
-sim = data + 'tbru.sim/'
+sim = data + 'sim/'
 simdata = sim + 'datasets/'
 
 def simresults(dsetname, simname):
